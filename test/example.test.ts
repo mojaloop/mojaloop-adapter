b502e66b9564a5a8b0d84220780a58104c87a5cf
DonChangfoot <donovanchangfoot@gmail.com>
2019-10-28 09:36:38 +0200

    chore: add db setup to example test

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,6 +1,30 @@
 import { generateOTP } from '../src/utils/util'
+import Knex = require('knex')
 
 describe('Example test', function () {
+  let knex: Knex
+
+  beforeAll(async () => {
+    knex = Knex({
+      client: 'sqlite3',
+      connection: {
+        filename: ':memory:',
+        supportBigNumbers: true
+      }
+    })
+  })
+
+  beforeEach(async () => {
+    await knex.migrate.latest()
+  })
+
+  afterEach(async () => {
+    await knex.migrate.rollback()
+  })
+
+  afterAll(async () => {
+    await knex.destroy()
+  })
 
   test('can generate a random OTP', async () => {
     const otp = generateOTP()
