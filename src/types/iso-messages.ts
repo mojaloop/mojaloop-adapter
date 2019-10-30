6a0831ba3e71eb32f8f6c1b56de65df6e26e45f0
DonChangfoot <donovanchangfoot@gmail.com>
2019-10-30 13:52:41 +0200

    feat: iso0100 type and test factory

---
+++ src/types/iso-messages.ts
@@ -0,0 +1,13 @@
+export type ISO0100 = {
+  3: string; // Processing code Transaction type – 01
+  4: string; // Amount
+  7: string; // Transmission date & time
+  11: string; // STAN
+  28: string; // Amount transaction fee
+  37: string; // RRN
+  41: string; // Card acceptor terminal identification
+  42: string; // Card acceptor identification code
+  49: string; // Currency code
+  102: string; // Mobile number/Account Number
+  103: string; // Account identification 2
+}
---
+++ test/factories/iso-messages.ts
@@ -0,0 +1,42 @@
+import { Factory } from 'rosie'
+import Faker from 'faker'
+import { ISO0100 } from '../../src/types/iso-messages'
+
+function pad (value: string, length: number, char: string) {
+  if (value.length >= length) {
+    return value.substring(0, length)
+  }
+
+  const diff = length - value.length
+  let padding = ''
+
+  for (let i = 0; i < diff; i++) {
+    padding += char
+  }
+
+  return padding + value
+}
+
+function generateField7 (): string {
+  const now = new Date(Date.now())
+  const month = (now.getUTCMonth() + 1).toString()
+  const day = now.getUTCDate().toString()
+  const hours = now.getUTCHours().toString()
+  const seconds = now.getUTCSeconds().toString()
+
+  return pad(month, 2, '0') + pad(day, 2, '0') + pad(hours, 2, '0') + pad(seconds, 2, '0')
+}
+
+export const ISO0100Factory = Factory.define<Partial<ISO0100>>('UserInfoFactory').attrs({
+  3: '012000',
+  4: '000000010000',
+  49: '820',
+  7: generateField7(),
+  37: Faker.internet.password(12, false, /[0-9a-z]/),
+  41: Faker.internet.password(8, false, /[0-9a-z]/),
+  42: Faker.internet.password(15, false, /[0-9a-z]/),
+  102: () => '26' + Faker.internet.password(26, false, /[0-9]/),
+  28: 'C00000001',
+  103: () => '04' + Faker.internet.password(6, false, /[0-9]/),
+  11: Faker.internet.password(6, false, /[0-9]/)
+})
