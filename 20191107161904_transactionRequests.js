de895e239e605015f3037f4d41bbbc8868588adc
appliedpayments <45846265+appliedpayments@users.noreply.github.com>
2019-11-08 16:36:57 +0530

    Add files via upload

---
+++ 20191107161904_transactionRequests.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+    return knex.schema
+           .createTable('transactionRequests', function (table) {
+            table.string('id',36).primary()
+            table.string('transactionId')
+            table.string('stan')
+            table.string('amount')
+            table.string('currency',3)
+            table.integer('expiration')
+            table.integer('createdAt')
+            table.integer('updatedAt')
+           })
+        };
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('transactionRequests')                             
+  
+};
+
+
+      
+      
