26d43382f336f2a2af6fd60a8cadc51a95c06cce
appliedpayments <45846265+appliedpayments@users.noreply.github.com>
2019-11-08 16:37:52 +0530

    Add files via upload

---
+++ migrations/20191108123750_transactionParties.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex) {
+
+    return knex.schema
+           .createTable('transactionParties', function (table) {
+            table.string('id',36).primary()
+            table.string('transactionRequestId')
+            table.string('type')
+            table.string('identifierType')
+            table.string('identifier')
+            table.string('fspid')
+            table.string('subIdorType')
+            table.integer('createdAt')
+            table.integer('updatedAt')
+            table.foreign('transactionRequestId').references('id').inTable('transactionRequests')
+            
+           })
+  
+};
+
+
+
+exports.down = function(knex) {
+
+    return knex.schema.dropTableIfExists('transactionParties')
+
+};
