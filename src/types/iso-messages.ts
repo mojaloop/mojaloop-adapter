2593a54312b273863bf9f9cb286fd09199494dae
DonChangfoot <donovanchangfoot@gmail.com>
2019-11-13 10:01:10 +0530

    feat: iso-messages-service and tests

--- src/types/iso-messages.ts
+++ src/types/iso-messages.ts
@@ -1,5 +1,6 @@
 export type ISO0100 = {
-  0: string;
+  id: number;
+  0: string; // MTI
   3: string; // Processing code Transaction type – 01
   4: string; // Amount
   7: string; // Transmission date & time
@@ -12,3 +13,21 @@ export type ISO0100 = {
   102: string; // Mobile number/Account Number
   103: string; // Account identification 2
 }
+
+export type ISO0110 = {
+  id: number;
+  0: string; // MTI
+  3: string; // Processing code Transaction type – 01
+  7: string; // Transmission date & time
+  11: string; // STAN
+  28: string; // Amount transaction fee
+  30: string; // Transaction processing fee
+  39: string; // Response code
+  41: string; // Card acceptor terminal identification
+  42: string; // Card acceptor identification code
+  48: string; // Amount transaction
+  49: string; // Currency code
+  102: string; // Mobile number/Account Number
+}
+
+export type ISOMessage = ISO0100 | ISO0110
--- test/factories/iso-messages.ts
+++ test/factories/iso-messages.ts
@@ -1,6 +1,6 @@
 import { Factory } from 'rosie'
 import Faker from 'faker'
-import { ISO0100 } from '../../src/types/iso-messages'
+import { ISO0100, ISO0110 } from '../../src/types/iso-messages'
 
 function pad (value: string, length: number, char: string) {
   if (value.length >= length) {
@@ -40,4 +40,19 @@ export const ISO0100Factory = Factory.define<Partial<ISO0100>>('Iso0100Factory')
   28: 'C00000001',
   103: () => '04' + Faker.internet.password(6, false, /[0-9]/),
   11: Faker.internet.password(6, false, /[0-9]/)
+})
+
+export const ISO0110Factory = Factory.define<Partial<ISO0110>>('Iso0110Factory').attrs({
+  0: '0110',
+  3: '012000',
+  7: generateField7(),
+  11: Faker.internet.password(6, false, /[0-9]/),
+  28: 'C00000001',
+  30: 'C00000001',
+  39: '00',
+  41: Faker.internet.password(8, false, /[0-9a-z]/),
+  42: Faker.internet.password(15, false, /[0-9a-z]/),
+  48: '012000',
+  49: '840',
+  102: () => '26' + Faker.internet.password(26, false, /[0-9]/)
 })
