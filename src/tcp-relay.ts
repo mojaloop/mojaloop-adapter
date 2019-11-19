4c2c7e03e52e8c5d4b0524f2908d3256de7cde6b
DonChangfoot <donovanchangfoot@gmail.com>
2019-11-19 15:45:40 +0530

    chore: add logging to tcp-relay

--- src/tcp-relay.ts
+++ src/tcp-relay.ts
@@ -6,7 +6,8 @@ const IsoParser = require('iso_8583')
 export function handleIsoMessage (data: Buffer, adaptor: Server): void {
   const mti = data.slice(2, 6).toString()
   const isoMessage = new IsoParser().getIsoJSON(data)
-
+  adaptor.app.logger.debug('Message mti: ' + mti)
+  adaptor.app.logger.debug('Message converted to JSON: ' + JSON.stringify(isoMessage))
   switch (mti) {
     case '0100':
       adaptor.inject({
@@ -24,12 +25,19 @@ export function createTcpRelay (adaptor: Server): net.Server {
 
   return net.createServer(function (sock) {
 
+    adaptor.app.logger.info('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort)
+
     const isoMessagingClient = new TcpIsoMessagingClient(sock)
 
     adaptor.app.isoMessagingClient = isoMessagingClient
 
     sock.on('data', (data) => {
-      handleIsoMessage(data, adaptor)
+      try {
+        adaptor.app.logger.debug('Received buffer message: ' + data.toString())
+        handleIsoMessage(data, adaptor)
+      } catch (error) {
+        adaptor.app.logger.error('Failed to handle iso message. ' + error.toString())
+      }
     })
 
   })
