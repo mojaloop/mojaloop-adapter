1ace25bb60b2e798946466fdf114c1b6038c34d3
DonChangfoot <donovanchangfoot@gmail.com>
2019-11-20 12:50:39 +0530

    chore: fix code formatting

--- migrations/20191107161904_transactionRequests.js
+++ migrations/20191107161904_transactionRequests.js
@@ -1,16 +1,15 @@
-exports.up = function(knex) {
-    return knex.schema
-        .createTable('transactionRequests', function(table) {
-            table.string('id', 36).primary()
-            table.string('transactionId')
-            table.string('stan')
-            table.string('amount')
-            table.string('currency', 3)
-            table.integer('expiration')
-            table.timestamps(true, true)
-        })
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('transactionRequests', function (table) {
+      table.string('id', 36).primary()
+      table.string('transactionId')
+      table.string('stan')
+      table.string('amount')
+      table.string('currency', 3)
+      table.integer('expiration')
+      table.timestamps(true, true)
+    })
 }
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('transactionRequests')
-
-};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('transactionRequests')
+}
