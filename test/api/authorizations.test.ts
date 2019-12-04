39c5dde26df8e7be52a5db7c1a9ce5ed37e3d6ef
ReeshmaPP <sumixavier@gmail.com>
2019-12-04 15:03:32 +0530

    feat: Modified 0110 message

--- test/api/authorizations.test.ts
+++ test/api/authorizations.test.ts
@@ -5,6 +5,7 @@ import { createApp } from '../../src/adaptor'
 import { Server } from 'hapi'
 import { AdaptorServicesFactory } from '../factories/adaptor-services'
 import { ISO0100Factory } from '../factories/iso-messages'
+import {ISO0110Factory} from '../factories/iso-messages'
 import { Socket } from 'net'
 import { TcpIsoMessagingClient } from '../../src/services/iso-messaging-client'
 import { KnexIsoMessageService } from '../../src/services/iso-message-service'
@@ -45,6 +46,7 @@ describe('Transaction Request Service', function () {
   })
 
   beforeEach(async () => {
+
     adaptor.app.isoMessagingClients.set('postillion', tcpIsoMessagingClient) // Registering Client
     await knex.migrate.latest()
     // this is the iso0100 message first being sent
@@ -65,7 +67,8 @@ describe('Transaction Request Service', function () {
     await knex.destroy()
   })
 
-  test('can test a fetch request', async () => {
+  test('returns a 200', async () => {
+
     const url = `/authorizations/${123}`
     const response1 = await adaptor.inject({
       method: 'GET',
@@ -81,11 +84,33 @@ describe('Transaction Request Service', function () {
       url: url
     })
 
+
+    const iso0110 = ISO0110Factory.build()
+    iso0110.id = 2
+    iso0110.transactionRequestId = '123'
+    iso0110.lpsKey = 'postillion:0100'
+    iso0110.lpsId = 'postillion'
+
     const isoMessageService = adaptor.app.isoMessagesService
     const iso0110JsonMessage = await isoMessageService.get('123', LPS_KEY, '0110')
+    
+    expect(iso0110[0]).toBe(iso0110JsonMessage[0])
+    expect(iso0110[3]).toBe(iso0110JsonMessage[3])
+    expect(iso0110[4]).toBe(iso0110JsonMessage[4])
+    expect(iso0110[28]).toBe(iso0110JsonMessage[28])
+    expect(iso0110[39]).toBe(iso0110JsonMessage[39])
+    expect(iso0110[49]).toBe(iso0110JsonMessage[49])
+    expect(iso0110[127.2]).toBe(iso0110JsonMessage[127.2])
+    expect(iso0110.id).toBe(iso0110JsonMessage.id)
+    expect(iso0110.transactionRequestId).toBe(iso0110JsonMessage.transactionRequestId)
+    expect(iso0110.lpsKey).toBe(iso0110JsonMessage.lpsKey)
+    expect(iso0110.lpsId).toBe(iso0110JsonMessage.lpsId)
+    
+
     const expectedBuffer = new IsoParser(iso0110JsonMessage).getBufferMessage()
 
     expect(expectedBuffer).toBeInstanceOf(Buffer)
     expect(sock.write).toHaveBeenCalledWith(expectedBuffer)
+
   })
 })
