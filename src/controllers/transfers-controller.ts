00678253f93a588c6fa228ed478b35f36fe45d6a
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-12-06 17:07:04 +0200

    transfers controller WIP

--- src/controllers/transfers-controller.ts
+++ src/controllers/transfers-controller.ts
@@ -1,13 +1,34 @@
+import { Request, ResponseToolkit, ResponseObject } from 'hapi'
+import { TransfersPostRequest } from 'types/mojaloop';
+
 const sdk = require('@mojaloop/sdk-standard-components')
 const IlpPacket = require('ilp-packet'); 
 
 
+// export async function create (request: Request, h: ResponseToolkit): Promise<ResponseObject> {
 
-// POST/transfer
-// use transactionId to find transaction
-// create transfer
-// calculate fulfilment
-// mojaloop/PUT/transfer/<Id>
+// 	request.server.app.logger.info('Received POST transfer request. headers: ' + JSON.stringify(request.headers) + ' payload: ' + JSON.stringify(request.payload))
+// 	const transferRequest = request.payload as TransfersPostRequest
+// 	const ilpPacket = transferRequest.ilpPacket
+// 	const binaryPacket = Buffer.from(ilpPacket, 'base64');
+// 	const jsonPacket = IlpPacket.deserializeIlpPacket(binaryPacket);
+	
+// 	const dataElement = JSON.parse(Buffer.from(jsonPacket.data.data.toString('utf8'), 'base64').toString('utf8'));
+// 	const transactionId = transferRequest.trans
+	
+// // extract transactionId from ilpPacket
+// // use transactionId to find transaction
+// // calculate fulfilment
+// // create transfer
+// // mojaloop/PUT/transfer/<Id>
+
+// }
+
+export async function playing (something: Something) {
+	// gets something
+	// does something
+	// return something
+}
 
 // PUT/transfer/<Id>
 // find transaction
