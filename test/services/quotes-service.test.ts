3e13543f9a076db5b0d342686b821f6031d7fb61
DonChangfoot <donovanchangfoot@gmail.com>
2019-12-06 16:00:28 +0200

    refactor!: quote service creates quote response
    * create calculates transferAmount and ilp related fields
    * takes in secret, function to calculate adaptor fees

--- test/services/quotes-service.test.ts
+++ test/services/quotes-service.test.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex'
 import Axios, { AxiosInstance } from 'axios'
 import { KnexQuotesService, DBQuote } from '../../src/services/quotes-service'
-import { QuotesPostRequest, Money } from '../../src/types/mojaloop'
+import { Money } from '../../src/types/mojaloop'
 import { QuotesPostRequestFactory } from '../factories/mojaloop-messages'
 
 describe('Quotes service', function () {
@@ -19,8 +19,8 @@ describe('Quotes service', function () {
       },
       useNullAsDefault: true
     })
-
-    quotesService = new KnexQuotesService(knex, fakeHttpClient)
+    const fakeLogger = { log: jest.fn() }
+    quotesService = new KnexQuotesService(knex, fakeHttpClient, 'secret', fakeLogger)
   })
 
   beforeEach(async () => {
@@ -35,36 +35,104 @@ describe('Quotes service', function () {
     await knex.destroy()
   })
 
-  test('can create a quote', async () => {
-    const quoteRequest = QuotesPostRequestFactory.build()
-    const fees: Money = { amount: '1', currency: 'USD' }
-    const transferAmount: Money = { amount: '101', currency: 'USD' }
+  describe('create', () => {
+    test('stores quote', async () => {
+      Date.now = jest.fn().mockReturnValue(0)
+      const quoteRequest = QuotesPostRequestFactory.build({
+        amount: {
+          amount: '100',
+          currency: 'USD'
+        }
+      })
+      const fees: Money = { amount: '1', currency: 'USD' }
+      const commission: Money = { amount: '2', currency: 'USD' }
+      const transferAmount: Money = { amount: '103', currency: 'USD' }
 
-    const quote = await quotesService.create(quoteRequest, fees, transferAmount, 'testCondition')
+      await quotesService.create(quoteRequest, fees, commission)
 
-    const dbQuote = await knex<DBQuote>('quotes').where('id', quoteRequest.quoteId).first()
+      const dbQuote = await knex<DBQuote>('quotes').where('id', quoteRequest.quoteId).first()
 
-    if (!dbQuote) {
-      fail('dbQuote does not exist')
-    }
-    expect(dbQuote.id).toBe(quoteRequest.quoteId)
-    expect(dbQuote.transactionId).toBe(quoteRequest.transactionId)
-    expect(dbQuote.amount).toBe(quoteRequest.amount.amount)
-    expect(dbQuote.amountCurrency).toBe(quoteRequest.amount.currency)
-    expect(dbQuote.feeAmount).toBe(fees.amount)
-    expect(dbQuote.feeCurrency).toBe(fees.currency)
-    expect(dbQuote.transferAmount).toBe(transferAmount.amount)
-    expect(dbQuote.transferAmountCurrency).toBe(transferAmount.currency)
-    expect(dbQuote.expiration).toBe(quoteRequest.expiration)
-    expect(dbQuote.condition).toBe('testCondition')
+      if (!dbQuote) {
+        fail('dbQuote does not exist')
+      }
+      expect(dbQuote.id).toBe(quoteRequest.quoteId)
+      expect(dbQuote.transactionId).toBe(quoteRequest.transactionId)
+      expect(dbQuote.amount).toBe(quoteRequest.amount.amount)
+      expect(dbQuote.amountCurrency).toBe(quoteRequest.amount.currency)
+      expect(dbQuote.feeAmount).toBe(fees.amount)
+      expect(dbQuote.feeCurrency).toBe(fees.currency)
+      expect(dbQuote.commission).toBe(commission.amount)
+      expect(dbQuote.commissionCurrency).toBe(commission.currency)
+      expect(dbQuote.transferAmount).toBe(transferAmount.amount)
+      expect(dbQuote.transferAmountCurrency).toBe(transferAmount.currency)
+      expect(dbQuote.expiration).toEqual((new Date(Date.now() + 10000)).toUTCString())
+      expect(dbQuote.condition).toBeDefined()
+      expect(dbQuote.ilpPacket).toBeDefined()
+    })
 
-    expect(quote.amount).toEqual(quoteRequest.amount)
-    expect(quote.id).toEqual(quoteRequest.quoteId)
-    expect(quote.transactionId).toEqual(quoteRequest.transactionId)
+    test('calculates transferAmount', async () => {
+      const quoteRequest = QuotesPostRequestFactory.build({
+        amount: {
+          amount: '100',
+          currency: 'USD'
+        }
+      })
+      const fees: Money = { amount: '1', currency: 'USD' }
+      const commission: Money = { amount: '2', currency: 'USD' }
+      const transferAmount: Money = { amount: '103', currency: 'USD' }
 
-    expect(quote.feeAmount).toEqual(fees)
-    expect(quote.transferAmount).toEqual(transferAmount)
-    expect(quote.condition).toBe('testCondition')
+      const quote = await quotesService.create(quoteRequest, fees, commission)
+
+      expect(quote.transferAmount).toMatchObject(transferAmount)
+    })
+
+    test('generates ilpPacket and condition', async () => {
+      const quoteRequest = QuotesPostRequestFactory.build({
+        amount: {
+          amount: '100',
+          currency: 'USD'
+        }
+      })
+      const fees: Money = { amount: '1', currency: 'USD' }
+      const commission: Money = { amount: '2', currency: 'USD' }
+
+      const quote = await quotesService.create(quoteRequest, fees, commission)
+
+      expect(quote.condition).toBeDefined()
+      expect(quote.ilpPacket).toBeDefined()
+    })
   })
 
+  test('can get a quote', async () => {
+    const quoteRequest = QuotesPostRequestFactory.build({
+      amount: {
+        amount: '100',
+        currency: 'USD'
+      }
+    })
+    const fees: Money = { amount: '1', currency: 'USD' }
+    const commission: Money = { amount: '2', currency: 'USD' }
+    const transferAmount: Money = { amount: '103', currency: 'USD' }
+    await quotesService.create(quoteRequest, fees, commission)
+
+    const quote = await quotesService.get(quoteRequest.quoteId, 'id')
+
+    expect(quote.amount).toMatchObject(quoteRequest.amount)
+    expect(quote.fees).toMatchObject(fees)
+    expect(quote.commission).toMatchObject(commission)
+    expect(quote.id).toEqual(quoteRequest.quoteId)
+    expect(quote.transactionId).toEqual(quoteRequest.transactionId)
+    expect(quote.transferAmount).toEqual(transferAmount)
+  })
+
+  test('calculates adaptor fees as 0 if calculateAdaptorFees is not registered', async () => {
+    const amount: Money = {
+      amount: '100',
+      currency: 'USD'
+    }
+
+    const adaptorFees = await quotesService.calculateAdaptorFees(amount)
+
+    expect(adaptorFees).toEqual({ amount: '0', currency: 'USD' })
+  })
 })
