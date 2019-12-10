d5a6f456c74571a003c60ba7156c86b4204de8d7
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-12-10 15:21:42 +0200

    transfers controller WIP

--- src/controllers/transfers-controller.ts
+++ src/controllers/transfers-controller.ts
@@ -1,13 +1,47 @@
+import { Request, ResponseObject, ResponseToolkit } from 'hapi';
+import IlpPacket from 'ilp-packet';
+import { TransfersPostRequest } from 'types/mojaloop';
+import { Transfer, KnexTransfersService } from 'services/transfers-service';
+
 const sdk = require('@mojaloop/sdk-standard-components')
-const IlpPacket = require('ilp-packet'); 
+
+export async function create (request: Request, h: ResponseToolkit): Promise<ResponseObject> {
+
+  const payload: TransfersPostRequest = request.payload as TransfersPostRequest
+
+  const binaryPacket = Buffer.from(payload.ilpPacket, 'base64');
+	const jsonPacket = IlpPacket.deserializeIlpPacket(binaryPacket);
+	const dataElement = JSON.parse(Buffer.from(jsonPacket.data.toString(), 'base64').toString('utf8'));
+  const transactionId = dataElement.transactionId
+
+  // use transactionId to find transaction
+
+  const fulfilment = sdk.Ilp.caluclateFulfil(payload.ilpPacket)
 
 
+  // create transfer
+  
+  const transfer: Transfer = {
+    id: "",
+    quoteId: "",
+    transactionRequestId: "",
+    fulfilment: "",
+    transferState: "",
+    amount: {
+      amount: "",
+      currency: "",
+    }
+  }
 
-// POST/transfer
-// use transactionId to find transaction
-// create transfer
-// calculate fulfilment
-// mojaloop/PUT/transfer/<Id>
+  await request.server.app.transfersService.create(transfer)
+  
+  // mojaloop/PUT/transfer/<Id>
+
+
+  return h.response().code(500)
+
+}
+
 
 // PUT/transfer/<Id>
 // find transaction
