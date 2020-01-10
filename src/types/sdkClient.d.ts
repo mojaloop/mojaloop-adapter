30f98a392b75df0c3d7db33442b8b881bc048143
jadinek <jkruger@student.wethinkcode.co.za>
2020-01-10 10:32:36 +0200

    updated sdkClient

--- src/types/sdkClient.d.ts
+++ src/types/sdkClient.d.ts
@@ -3,71 +3,71 @@
 
 declare module '@mojaloop/sdk-standard-components' {
 
-    type PartyIdInfo = {
-      partyIdType: string;
-      partyIdentifier: string;
-      partySubIdOrType?: string;
-      fspId: string;
-    }
+  type PartyIdInfo = {
+    partyIdType: string;
+    partyIdentifier: string;
+    partySubIdOrType?: string;
+    fspId: string;
+  }
 
-    type BulkParticipantsRequest = {
-      requestId: string;
-      partyList: Array<PartyIdInfo>;
-      currency?: string;
-    }
+  type BulkParticipantsRequest = {
+    requestId: string;
+    partyList: Array<PartyIdInfo>;
+    currency?: string;
+  }
 
-    type Money = {
-      currency: string;
-      amount: string;
-    }
+  type Money = {
+    currency: string;
+    amount: string;
+  }
 
-    type PostTransferBody = {
-      transferId: string;
-      payeeFsp: string;
-      payerFsp: string;
-      amount: Money;
-      ilpPacket: string;
-      condition: string;
-      expiration: string;
-    }
+  type PostTransferBody = {
+    transferId: string;
+    payeeFsp: string;
+    payerFsp: string;
+    amount: Money;
+    ilpPacket: string;
+    condition: string;
+    expiration: string;
+  }
 
-    type Config = {
-      logger: any;
-      dfspId: string;
-      tls: {
-        outbound: {
-          mutualTLS: {
-            enabled: boolean;
-          };
+  type Config = {
+    logger: any;
+    dfspId: string;
+    tls: {
+      outbound: {
+        mutualTLS: {
+          enabled: boolean;
         };
       };
-      jwsSign: boolean;
-      jwsSignPutParties?: boolean;
-      jwsSigningKey: string;
-      peerEndpoint: string;
-      alsEndpoint?: string;
-      quotesEndpoint?: string;
-      transfersEndpoint?: string;
-      wso2Auth?: any;
-    }
-
-    class MojaloopRequests {
-      constructor(config: Config)
-
-      getParties(idType: string, idValue: string): Promise<object>
-      putParties(idType: string, idValue: string, body: object, destFspId: string): Promise<object>
-      putPartiesError(idType: string, idValue: string, error: object, destFspId: string): Promise<object>
-
-      postParticipants(request: BulkParticipantsRequest, destFspId?: string): Promise<object>
-      putParticipants(idType: string, idValue: string, body: object, destFspId: string): Promise<object>
-      putParticipantsError(idType: string, idValue: string, error: object, destFspId: string): Promise<object>
-
-      postQuotes(quoteRequest: object, destFspId: string): Promise<object>
-      putQuotes(quoteId: string, quoteResponse: object, destFspId: string): Promise<object>
-      putQuotesError(quoteId: string, error: object, destFspId: string): Promise<object>
-
-      postTransfers(prepare: PostTransferBody, destFspId: string): Promise<object>
-      putTransfers(transferId: string, fulfilment: object, destFspId: string): Promise<object>
-      putTransfersError(transferId: string, error: object, destFspId: string): Promise<object>
-    }
+    };
+    jwsSign: boolean;
+    jwsSignPutParties?: boolean;
+    jwsSigningKey: string;
+    peerEndpoint: string;
+    alsEndpoint?: string;
+    quotesEndpoint?: string;
+    transfersEndpoint?: string;
+    wso2Auth?: any;
   }
+
+  class MojaloopRequests {
+    constructor(config: Config)
+
+    getParties(idType: string, idValue: string, idSubValue: string | null): Promise<object>
+    putParties(idType: string, idValue: string, idSubValue: string | null, body: object, destFspId: string): Promise<object>
+    putPartiesError(idType: string, idValue: string, idSubValue: string | null, error: object, destFspId: string): Promise<object>
+
+    postParticipants(request: BulkParticipantsRequest, destFspId?: string): Promise<object>
+    putParticipants(idType: string, idValue: string, idSubValue: string | null, body: object, destFspId: string): Promise<object>
+    putParticipantsError(idType: string, idValue: string, idSubValue: string | null, error: object, destFspId: string): Promise<object>
+
+    postQuotes(quoteRequest: object, destFspId: string): Promise<object>
+    putQuotes(quoteId: string, quoteResponse: object, destFspId: string): Promise<object>
+    putQuotesError(quoteId: string, error: object, destFspId: string): Promise<object>
+
+    postTransfers(prepare: PostTransferBody, destFspId: string): Promise<object>
+    putTransfers(transferId: string, fulfilment: object, destFspId: string): Promise<object>
+    putTransfersError(transferId: string, error: object, destFspId: string): Promise<object>
+  }
+}
