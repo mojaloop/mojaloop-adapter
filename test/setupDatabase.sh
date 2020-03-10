46c68652002ced4a0a5351e5e0dc191b3968dad1
DonChangfoot <donovanchangfoot@gmail.com>
2020-03-10 14:09:15 +0200

    fix: wait for mysql to be ready

--- test/setupDatabase.sh
+++ test/setupDatabase.sh
@@ -1,3 +1,6 @@
 docker run -d --name ps -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 percona/percona-server:8.0
+# wait for mysql to be ready
+sleep 10
+
 docker exec -it ps mysql -u root -proot -e "ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root'"
 docker exec -it ps mysql -u root -proot -e "CREATE DATABASE testing;"
